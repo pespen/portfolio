f76cc16e45ca3dd00c3ab4b4a12e45ab509c27f1
pespen <pederespen@gmail.com>
2025-03-26 21:37:01 +0100

    added basic code for a portfolio, tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Replace with your GitHub username
+  site: "https://yourusername.github.io",
+
+  // Set to '/' since this will be deployed to the main GitHub Pages URL
+  base: "/",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
